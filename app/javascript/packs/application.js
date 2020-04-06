d33acd602e544c3347d01c0e5d1a986161df2044
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-04-06 15:20:31 +0200

    test javascript rien donné

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,19 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+const champ = document.getElementById("name");
+champ.addEventListener("keyup", e => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+  }
+  search(e.currentTarget.value);
+});
+
+const search = (query) => {
+  fetch("https://www.mtgjson.com/files/AllPrintings.json")
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data);
+  });
+}
