44ae04817ff5d3f6493f1e1856cbcecd1ff8d76f
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-04-14 17:34:48 +0200

    wantform v2 done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,19 +4,19 @@ require("channels")
 
 import "bootstrap";
 
-const champ = document.getElementById("name");
-champ.addEventListener("keyup", e => {
-  if (event.isComposing || event.keyCode === 229) {
-    return;
-  }
-  search(e.currentTarget.value);
-});
+// const champ = document.getElementById("name");
+// champ.addEventListener("keyup", e => {
+//   if (event.isComposing || event.keyCode === 229) {
+//     return;
+//   }
+//   search(e.currentTarget.value);
+// });
 
-const search = (query) => {
-  fetch("https://www.mtgjson.com/files/AllPrintings.json")
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-  });
-}
+// const search = (query) => {
+//   fetch("https://www.mtgjson.com/files/AllPrintings.json")
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
+// }
 
