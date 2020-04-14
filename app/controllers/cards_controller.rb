821a2e2cb5d1c7907d31cd6b4c9389b5475c512e
thibault rahou <trahou33@gmail.com>
2020-04-14 16:59:43 +0200

    crud card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,35 @@
 class CardsController < ApplicationController
+
+  def index
+    @cards = Card.all
+  end
+
+  def new
+  end
+
+  def edit
+    @card = Card.new
+  end
+
+
+
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to @card
+    else
+      redirect_to  edit_card_path
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def cards_params
+    params.require(:card).permit(:condition)
+  end
 end
