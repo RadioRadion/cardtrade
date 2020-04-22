16c53bcac10b09acf294b53ac9082dcb1c618b13
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-04-22 16:57:57 +0200

    user show done, card#new added

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,14 +5,25 @@ class CardsController < ApplicationController
   end
 
   def new
-     @card = Card.new
+    require 'csv'
+    @card = Card.new
+
+    @cards = []
+    @names = []
+    filepath = 'lib/datas/cards.csv'
+    CSV.foreach(filepath) do |row|
+    # Here, row is an array of columns. 46 => name, 59 => setCode
+      @cards << [row[46], row[59]]
+      @names << row[46]
+    end
+    @uniqsName = @names.uniq.sort
   end
 
   def create
     @card = Card.new(cards_params)
     @card.user = current_user
     if @card.save!
-      redirect_to cards_path
+      redirect_to user_path(current_user)
     else
       render :new
     end
@@ -27,7 +38,7 @@ class CardsController < ApplicationController
     @card = Card.find(params[:id])
 
     if @card.update(cards_params)
-      redirect_to cards_path
+      redirect_to user_path(current_user)
     else
       render :new
 
@@ -37,13 +48,13 @@ class CardsController < ApplicationController
   def destroy
     @card = Card.find(params[:id])
     @card.destroy
-    redirect_to cards_path
+    redirect_to user_path(current_user)
   end
 
   private
 
   def cards_params
-    params.require(:card).permit(:condition)
+    params.require(:card).permit(:name, :quantity, :extension, :foil, :condition, :language)
   end
 end
 
