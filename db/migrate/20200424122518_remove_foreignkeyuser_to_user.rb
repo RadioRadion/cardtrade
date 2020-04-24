18b204e6457759bfc38629497738f51ee4c7bc17
thibault rahou <trahou33@gmail.com>
2020-04-24 15:16:21 +0200

    fixbasededonnée2

--- db/migrate/20200424122518_remove_foreignkeyuser_to_user.rb
+++
@@ -1,5 +0,0 @@
-class RemoveForeignkeyuserToUser < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :users, name: :users
-  end
-end
--- db/migrate/20200424123517_remove_usersid_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUsersidToUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :users, column: :users_id
-  end
-end
